ee010ccd4b177057ed03f493f8b80f696f151f56
selvan01 <131957941+selvan01@users.noreply.github.com>
2024-02-28 21:42:08 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
+
 from chat import get_response
 
-app=Flask(__name__)
-CORS(app)
+app = Flask(__name__)
+
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
 
 @app.post("/predict")
 def predict():
