de11e431f0799b84f7af07971e08f5cba3364ceb
S E L V A N <131957941+selvan01@users.noreply.github.com>
2024-04-11 17:33:33 +0530

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,324 @@
+class Chatbox {
+    constructor() {
+        this.args = {
+            openButton: document.querySelector('.chatbox__button'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button')
+        }
+
+        this.state = false;
+        this.messages = [];
+    }
+
+    display() {
+        const {openButton, chatBox, sendButton} = this.args;
+
+        openButton.addEventListener('click', () => this.toggleState(chatBox))
+
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+
+        const node = chatBox.querySelector('input');
+        node.addEventListener("keyup", ({key}) => {
+            if (key === "Enter") {
+                this.onSendButton(chatBox)
+            }
+        })
+    }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+
+        // show or hides the box
+        if(this.state) {
+            chatbox.classList.add('chatbox--active')
+        } else {
+            chatbox.classList.remove('chatbox--active')
+        }
+    }
+
+    onSendButton(chatbox) {
+        var textField = chatbox.querySelector('input');
+        let text1 = textField.value
+        if (text1 === "") {
+            return;
+        }
+
+        let msg1 = { name: "User", message: text1 }
+        this.messages.push(msg1);
+
+        fetch('http://127.0.0.1:5000/predict', {
+            method: 'POST',
+            body: JSON.stringify({ message: text1 }),
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          })
+          .then(r => r.json())
+          .then(r => {
+            let msg2 = { name: "Sam", message: r.answer };
+            this.messages.push(msg2);
+            this.updateChatText(chatbox)
+            textField.value = ''
+
+        }).catch((error) => {
+            console.error('Error:', error);
+            this.updateChatText(chatbox)
+            textField.value = ''
+          });
+    }
+
+    updateChatText(chatbox) {
+        var html = '';
+        this.messages.slice().reverse().forEach(function(item, index) {
+            if (item.name === "Sam")
+            {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
+            }
+            else
+            {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
+            }
+          });
+
+        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        chatmessage.innerHTML = html;
+    }
+}
+
+
+const chatbox = new Chatbox();
+chatbox.display();
+
+
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    // Records the initial cursor and scroll position of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    if(!isDragging) return; // if isDragging is false return from here
+    // Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+    // Autoplay the carousel after every 2500 ms
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+
+
+  let li = document.querySelectorAll(".faq-text li");
+    for (var i = 0; i < li.length; i++) {
+      li[i].addEventListener("click", (e)=>{
+        let clickedLi;
+        if(e.target.classList.contains("question-arrow")){
+          clickedLi = e.target.parentElement;
+        }else{
+          clickedLi = e.target.parentElement.parentElement;
+        }
+       clickedLi.classList.toggle("showAnswer");
+      });
+    }
+
+var voice=document.getElementById("vvv");
+var hear=document.getElementById("hear");
+
+var sendbtn=document.getElementById("sendbtn")
+
+sendbtn.addEventListener("click",function(){
+    
+    let lastname=hear.value;
+    let firstname= "";
+    
+    if(lastname == ""){
+        alert("enter the chat")
+    }
+    else{
+        setCookie(firstname,lastname,365)
+    }
+})
+
+function setCookie(name,value,time){
+    let d = new Date()
+
+    d.setTime(d.getTime() + (time*24*60*60*1000))
+
+    let expires = "expires =" + d.toUTCString()
+    
+    document.cookie = `${name}=${value};${expires};path='/'`
+
+   
+}
+function getCookie(){
+    let cookies = document.cookie.split(";")
+
+    let res;
+
+    for(let i=1;i<cookies.length;i++){
+        res+=i+ '-' + cookies [i-1] + "<br>"
+    }
+
+    document.getElementById("result").innerHTML+= res
+    
+}
+ let result=document.getElementById("result");
+ let getbutton=document.getElementById("getbtn");
+
+ getbutton.addEventListener("click",function(){
+    result.style.display="block"
+ })
+
+voice.addEventListener("click",function(){
+    var speech=true;
+    window.SpeechRecongnition=window.webkitSpeechRecognition;
+    const recognition= new SpeechRecongnition();
+    recognition.interimResults=true;
+  recognition.addEventListener("result",e=>{
+      const transcript= Array.from(e.results)
+      .map(result => result[0])
+      .map(result=>result.transcript)
+
+      hear.value=transcript;
+  })
+    if(speech==true){
+        recognition.start();
+    }
+})
+
+
+
+
+var speak = document.getElementById("btn");
+speak.addEventListener("click", () => {
+    var msg = "Welcome to Career Quest.";
+    const utterance = new SpeechSynthesisUtterance(msg);
+    utterance.rate = 1; // Standard speech rate (1x)
+    utterance.pitch = 1; // Standard pitch (1x)
+    utterance.volume = 1; // Standard volume (1x)
+    utterance.voice = speechSynthesis.getVoices().find(voice => voice.name === 'Karen'); // Use a female voice ('Karen' is an example, adjust according to available voices)
+    speechSynthesis.speak(utterance);
+
+    setTimeout(() => {
+        var msgtwo = "Career Quest is a comprehensive job suggestion platform designed to help individuals improve their careers.";
+        const voicenxt = new SpeechSynthesisUtterance(msgtwo);
+        voicenxt.rate = 1; // Standard speech rate (1x)
+        voicenxt.pitch = 1; // Standard pitch (1x)
+        voicenxt.volume = 1; // Standard volume (1x)
+        voicenxt.voice = speechSynthesis.getVoices().find(voice => voice.name === 'Karen'); // Use the same female voice
+        speechSynthesis.speak(voicenxt);
+    }, 2000); // Set pause between utterances to 2 seconds (2000 milliseconds)
+});
+
+
+
+
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+      let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+          }
+
+// js code to toggle dark and light mode
+      modeToggle.addEventListener("click" , () =>{
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+
+        // js code to keep user selected mode even page refresh or file reopen
+        if(!body.classList.contains("dark")){
+            localStorage.setItem("mode" , "light-mode");
+        }else{
+            localStorage.setItem("mode" , "dark-mode");
+        }
+      });
+
+// js code to toggle search box
+        searchToggle.addEventListener("click" , () =>{
+        searchToggle.classList.toggle("active");
+      });
+ 
+      
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});
+
+var about = document.querySelector(".right");
+modeToggle.addEventListener ("click" ,()=>{
+    about.style.backgroundImage="url('funny.gif')";
+});
+
+function change(){
+    about.body.style.backgroundImage="url('funny.gif')";
+}
+
